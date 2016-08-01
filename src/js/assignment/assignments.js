ac8c209fa20dab49fab446694fef46b2253558f9
Switzer <rjswitzer3@gmail.com>
2016-07-31 20:40:32 -0400

    Completed base schema

--- src/js/assignment/assignments.js
+++ src/js/assignment/assignments.js
@@ -1,11 +1,12 @@
 var assignmentCollection = [
   {
+    "_id": "e3968d58-29ef-7784-1ac9-75eaf5fa8ad6",
     "assignmentname": "HW I",
-    "courseId": "MATH-201",
+    "courseId": "d979c77d-0408-827d-153b-b6bf656e6d40",
     "teacher": "Mrs. Rack",
-    "teacherId": "",
+    "teacherId": "4cb295fe-e516-4a8b-4abd-94e5da43856c",
     "student": "John Doe",
-    "studentId":"",
+    "studentId": "3d3ad3ad-5bce-a672-fddf-bd57ba0190bd",
     "dueDate": "7/31/16",
     "submitDate": "7/21/16",
     "grade": "B",
--- src/js/student/students.js
+++ src/js/student/students.js
@@ -1,12 +1,22 @@
 var studentCollection = [
   {
+    "_id": "3d3ad3ad-5bce-a672-fddf-bd57ba0190bd",
     "username": "student",
     "firstname": "John",
     "lastname": "Doe",
     "password": "none",
     "email": "jdoe66@gmail.com",
     "points": "243",
-    "courses": ["Calculus I","Linear Algebra"],
+    "courses": [
+      {
+        "_id": "d979c77d-0408-827d-153b-b6bf656e6d40",
+        "coursename": "Calculus I",
+        "courseId": "MATH-201",
+        "teacher": "Mrs. Rack",
+        "teacherId": "",
+        "students": ["3d3ad3ad-5bce-a672-fddf-bd57ba0190bd",],
+        "assignments": ["HW1","HW2"],
+      },"Linear Algebra"],
     "school": "Rochester Institute of Technology",
     "city": "Rochester",
     "state": "NY",
