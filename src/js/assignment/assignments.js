2767baccce57752c271527738538498f0f2bf792
Ryan Switzer <rjs4244@rit.edu>
2016-07-31 20:17:20 -0400

    Completed schema outline

---
+++ src/js/assignment/assignments.js
@@ -0,0 +1,15 @@
+var assignmentCollection = [
+  {
+    "assignmentname": "HW I",
+    "courseId": "MATH-201",
+    "teacher": "Mrs. Rack",
+    "teacherId": "",
+    "student": "John Doe",
+    "studentId":"",
+    "dueDate": "7/31/16",
+    "submitDate": "7/21/16",
+    "grade": "B",
+    "points": "88"
+  },
+
+]
